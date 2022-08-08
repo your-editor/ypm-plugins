eb1bcbc01792ed3c7d697721e085a72b79ab1249
jmcmicha <jmcmicha@com1636.eecs.utk.edu>
2022-08-08 12:52:38 -0400

    updated update script

--- update_plugin.sh
+++ update_plugin.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 read -e -p "git submodule update --init --remote --merge " plugin_name
-git submodule update --init --remote --merge $plugin_name
-
+# git submodule update --init --remote --merge $plugin_name
+git submodule init $plugin_name
+git submodule update $plugin_name
+cwd=$(pwd)
+cd $plugin_name
+git checkout main
+git pull
+cd $cwd
 cp $plugin_name*.7 man/man7/
