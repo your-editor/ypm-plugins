5dc759f5b0dba39745b781b5e04fb3e53e8bbb51
jmcmicha <jmcmicha@com1636.eecs.utk.edu>
2022-08-09 13:47:14 -0400

    updated mouse_menu

--- update_plugin.sh
+++ update_plugin.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-read -e -p "git submodule update --init --remote --merge " plugin_name
+read -e -p "Directory to plugin you want to update:" plugin_name
 git submodule init $plugin_name
 git submodule update $plugin_name
 cwd=$(pwd)
 cd $plugin_name
-git checkout main
+git checkout main || git checkout master
 git pull
 cd $cwd
 cp $plugin_name*.7 man/man7/
